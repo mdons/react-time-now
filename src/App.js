399791a73e57b40916d619e7415dc7f6a83a2bac
mdons <michaeljdonnell@gmail.com>
2019-01-08 20:34:23 -0600

    finish the time app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import moment from "moment";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentTime: moment().format("LTS") };
+  }
+  showCurrentTime() {
+    let now = moment().format("LTS");
+    this.setState({ currentTime: now });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div onClick={() => this.showCurrentTime()} className="time-box">
+          <p> {this.state.currentTime} </p>
+        </div>
       </div>
     );
   }
